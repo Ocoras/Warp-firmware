718aee6c42baf8a5c41b97738dab321f641f9871
David Swarbrick <david.swarbrick9@gmail.com>
2020-01-02 19:20:05 +0000

    Beginning implementation of example ADC

--- src/boot/ksdk1.1.0/devAD8318.c
+++ src/boot/ksdk1.1.0/devAD8318.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-#include "fsl_spi_master_driver.h"
 #include "fsl_port_hal.h"
+#include "fsl_adc16_driver.h"
 
 #include "SEGGER_RTT.h"
 #include "gpio_pins.h"
@@ -21,8 +21,52 @@ enum
 	kAD8318Pin	= GPIO_MAKE_PIN(HW_GPIOB, 0),
 };
 
+// Function copied from Kinetis SDK 1.1 API Reference Manual 
+void ADC16_TEST_OneTimeTrigger(uint32_t instance, uint32_t chnGroup, uint8_t chn)
+{
+#if FSL_FEATURE_ADC16_HAS_CALIBRATION
+	adc16_calibration_param_t MyAdcCalibraitionParam;
+#endif // FSL_FEATURE_ADC16_HAS_CALIBRATION //
 
+	adc16_user_config_t MyAdcUserConfig;
+	adc16_chn_config_t MyChnConfig;
+	int32_t MyAdcValue;
+	uint32_t i;
 
+#if FSL_FEATURE_ADC16_HAS_CALIBRATION
+	// Auto calibration. //
+	ADC16_DRV_GetAutoCalibrationParam(instance, &MyAdcCalibraitionParam);
+	ADC16_DRV_SetCalibrationParam(instance, &MyAdcCalibraitionParam);
+#endif // FSL_FEATURE_ADC16_HAS_CALIBRATION //
+
+	// Initialize the ADC converter. //
+	ADC16_DRV_StructInitUserConfigDefault(&MyAdcUserConfig);
+	ADC16_DRV_Init(instance, &MyAdcUserConfig);
+
+	// Configuration for ADC channel. //
+	MyChnConfig.chnNum = chn;
+	MyChnConfig.diffEnable= false;
+	MyChnConfig.intEnable = false;
+
+	#if FSL_FEATURE_ADC16_HAS_MUX_SELECT
+	MyChnConfig.chnMux = kAdcChnMuxOfA;
+	#endif // FSL_FEATURE_ADC16_HAS_MUX_SELECT //
+
+	for (i = 0U; i < 4U; i++)
+	{
+		// Trigger the conversion with indicated channel’s configuration. //
+		ADC16_DRV_ConfigConvChn(instance, chnGroup, &MyChnConfig);
+		// Wait for the conversion to be done. //
+		ADC16_DRV_WaitConvDone(instance, chnGroup);
+		// Fetch the conversion value and format it. //
+		MyAdcValue = ADC16_DRV_GetConvValueRAW(instance, chnGroup);
+		// ADC16_DRV_ConvRAWData(MyAdcValue, false, kAdcResolutionBitOfSingleEndAs12) );
+	}
+	// Pause the conversion after testing. //
+	ADC16_DRV_PauseConv(instance, chnGroup);
+	// Disable the ADC. //
+	ADC16_DRV_Deinit(instance);
+}
 
 
 int
--- src/boot/ksdk1.1.0/devAD8318.h
+++ src/boot/ksdk1.1.0/devAD8318.h
@@ -1,7 +1,3 @@
-/*
- *	See https://github.com/adafruit/Adafruit-SSD1331-OLED-Driver-Library-for-Arduino for the Arduino driver.
- */
-
 // typedef enum
 // {
 // 	kAD8318ColororderRGB		= 1,
@@ -9,46 +5,30 @@
 // 	kAD8318DelaysHWLINE		= 1,
 // } AD8318Constants;
 
-int	devAD8318init(void);
-// void writeText(uint8_t c);
 
-//
-// class SSD1331 {
-// private:
-// 	uint8_t width;
-// 	uint8_t height;
-// 	uint8_t cursor_x;
-// 	uint8_t cursor_y;
-//
-// 	int writeCommand(uint8_t commandByte);
-// 	void drawRect(uint8_t start_x, uint8_t start_y, uint8_t width_x, uint8_t width_y, uint8_t * color_pointer);
-// 	void drawChar(uint8_t x, uint8_t y, uint16_t c, uint8_t * colour, uint8_t * bg, uint8_t size_x, uint8_t size_y);
-//
-//
-// public:
-// 	void writeText(uint8_t c);
-// 	int init(void);
-// 	uint8_t textsize_x;
-// 	uint8_t textsize_y;
-// 	uint8_t textcolor[3];
-// 	uint8_t textbg[3];
-// 	bool wrap;
-//
-// 	Screen(uint8_t w,	uint8_t h){
-// 		width = w;
-// 		height = h;
-// 		cursor_x = 0;
-// 		cursor_y = 0;
-// 		textsize_x = 1;
-// 		textsize_y = 1;
-// 		wrap = true;
-// 		textcolor[0] = 0x00 ;
-// 		// Green for hacker vibes
-// 		textcolor[1] = 0x3E ;
-// 		textcolor[2] = 0x00 ;
-// 		textbg[0] = 0x00;
-// 		textbg[1] = 0x00;
-// 		textbg[2] = 0x00;
-//
-// 	}
-// }
+// #ifndef __ADC16_TEST_H__
+// #define __ADC16_TEST_H__
+// #include
+// #include
+// #include
+// #include
+// #include
+// #include
+// <stdio.h>
+// <stdbool.h>
+// <stdint.h>
+// "board.h"
+// "fsl_os_abstraction.h"
+// "fsl_debug_console.h"
+// #include "fsl_adc16_driver.h"
+// #define ADC16_TEST_CHN_TEMPERATURE
+// #define ADC16_TEST_CHN_GROUT_NUM (26U) // Temperature Sensor. //
+// (0U)
+// #define ADC16_TEST_LOOP_CYCLE (4U)
+
+void ADC16_TEST_OneTimeTrigger(uint32_t instance, uint32_t chnGroup, uint8_t chn);
+
+// #endif // __ADC16_TEST_H__ //
+
+
+int	devAD8318init(void);
